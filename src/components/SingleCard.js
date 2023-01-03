5b3a05e8a1c09808795e97f57612c2204ae2666c
Håkan <hali0151@student.umu.se>
2023-01-03 22:36:45 +0100

    add deploy script

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -8,7 +8,7 @@ export default function SingleCard({ card, handleChoice, flipped, disabled }) {
       handleChoice(card);
     }
   }
-
+  console.log(card.src)
     return (
         <div className="card">
              <div className={flipped ? 'flipped' : ""}>
